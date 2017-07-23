aaa91a99f3b148ae2d953f4d06e894e04e8d50d0
tunnelshade <tunnelshade@gmail.com>
2017-07-24 02:44:13 +0530

    Updated makefile

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Bharadwaj Machiraju'
 SITENAME = 'bharadwaj.machiraju'
-SITEURL = 'http://tunnelshade.in/'
+SITEURL = 'http://tunnelshade.in'
 
 PATH = 'content'
 
