eaad2773183d994f95c2fdd79828668189186d75
tunnelshade <tunnelshade@gmail.com>
2017-07-24 13:24:15 +0530

    Updated colorscheme

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ DEFAULT_DATE_FORMAT = "%d-%m-%y"
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feed/all.atom.xml'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
